3d6459d93ac94c56adc3c9efb2b49ea3de03b459
Iffat Binte Sikder <isikder@my.centennialcollege.ca>
2024-12-01 22:35:31 -0500

    Initial commit

---
+++ ConsoleApp/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using StringExtensions;  // Import the namespace for the extension method
+
+class Program
+{
+    static void Main()
+    {
+        // Test the extension method
+        string testString = "Hello";
+
+        bool result = testString.StartsWithUpperCase();  // Use the extension method
+
+        Console.WriteLine($"Does the string start with an uppercase letter? {result}");
+    }
+}
+
+
--- StringExtensions/Class1.cs
+++ StringExtensions/Class1.cs
@@ -1,7 +1,19 @@
-﻿namespace StringExtensions;
+﻿using System;
 
-public class Class1
+namespace StringExtensions
 {
+    public static class StringExtensions
+    {
+        // Extension method to check if the string starts with an uppercase letter
+        public static bool StartsWithUpperCase(this string str)
+        {
+            // Check if the string is not null or empty and if the first character is uppercase
+            if (string.IsNullOrEmpty(str))
+                return false;
 
+            return char.IsUpper(str[0]);
+        }
+    }
 }
+
 
